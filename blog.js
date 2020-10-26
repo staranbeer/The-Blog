ae8778e30d8ac535942fea53b0a2a79dfa1ae8c0
taranbeer singh <62498540+staranbeer@users.noreply.github.com>
2020-10-26 14:51:26 +0530

    added some features to the blog page

--- blog.js
+++ blog.js
@@ -1,25 +1,68 @@
-const subHeading = document.getElementById('createSubHeading')
-const container = document.getElementById('post-container')
+const url = "";
 
-const post = []
+const blogContainer = document.getElementById('post-container')
 
-const title = document.getElementById('main-heading')
-const heading = document.getElementById('main-summary')
+const subheadingButton = document.getElementById('createSubheading')
+const paragraphButton = document.getElementById('createParagraph')
+const imageButton = document.getElementById('createImage')
+const conclusionButton = document.getElementById('createConclusion')
+const done = document.getElementById('done')
 
+var conclusionCounter = 0;
+var imageCounter = 0
+var subheadingCounter = 0;
+var paragraphCounter = 0;
 
-subHeading.addEventListener('click', (i)=>{
-    const heading = document.createElement('input')
-    heading.className = 'subheading'
-    heading.placeholder = "Subheading"
-    container.appendChild(heading)
-
+subheadingButton.addEventListener('click', ()=>{
+    const input = document.createElement('input')
+    input.className = "subheading"
+    input.name = "Subheading" + subheadingCounter
+    input.placeholder = "Sub heading"
+    subheadingCounter++
+    blogContainer.appendChild(input)
 })
 
-const paragraph = document.getElementById('paragraph')
 
-paragraph.addEventListener('click', ()=>{
-    const paragraph = document.createElement('textarea')
-    paragraph.id = 'main-summary'
-    container.appendChild(paragraph)
+
+paragraphButton.addEventListener('click', ()=>{
+    const input = document.createElement('textarea')
+    input.className = "paragraph"
+    input.placeholder = "Paragraph"
+    input.name = "paragraph" + paragraphCounter
+    paragraphCounter++
+    blogContainer.appendChild(input)
 })
 
+
+imageButton.addEventListener('click', ()=>{
+    const input = document.createElement('input')
+    input.type = "file"
+    input.className = "image"
+    input.name = "image" + imageCounter
+    imageCounter++
+    blogContainer.appendChild(input)
+})
+
+conclusionButton.addEventListener('click', ()=>{
+    const input = document.createElement('textarea')
+    input.className = "main-summary"
+    input.placeholder = "Conclusion"
+    input.name = "conclusion" + conclusionCounter
+    conclusionCounter++
+    blogContainer.appendChild(input)
+})
+
+
+
+done.addEventListener('click', ()=>{
+   
+       let form = new FormData(blogContainer)
+       for(var pair of form.entries()) {
+        console.log(pair); 
+     }
+     
+        fetch( url,{
+            method: 'POST',
+            body: form
+        } )
+    });
